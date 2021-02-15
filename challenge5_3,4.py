92c9b89841795fb8ca01fd245cedf89ad118658e
Umebosh1 <79074515+Umebosh1@users.noreply.github.com>
2021-02-15 17:30:55 +0900

    Add files via upload

---
+++ challenge5_3,4.py
@@ -0,0 +1,6 @@
+#challenge_3
+dict={"Height":156,"Favorite color":"Blue","Favorite author":"Riku Onda"}
+
+#challenge_4
+n=input("Height、Favorite color、Favorite authorから選んで入力してください")
+print(dict[n])
